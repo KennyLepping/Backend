4f71f90bc969fb40597f87739a8d49e9f0267477
Debian <bitnami@ip-172-26-15-37.ec2.internal>
2021-10-18 19:13:19 +0000

    First commit

---
+++ config/wsgi.py
@@ -0,0 +1,13 @@
+import os
+
+import sys
+
+sys.path.append('/opt/bitnami/projects/backend')
+
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/backend/egg_cache")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
