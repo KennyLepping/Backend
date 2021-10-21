6ab00c11b51e43cb0b23037fea7c67c893f05d01
Kenny Lepping <kennylepping@gmail.com>
2021-10-21 08:51:51 -0400

    Adding the rest of the files

--- config/settings/base.py
+++ config/settings/base.py
@@ -165,8 +165,8 @@ AWS_S3_CUSTOM_DOMAIN = 'd28gmzqzeakcc8.cloudfront.net'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ALLOW_CREDENTIALS = True
-# ACCESS_CONTROL_ALLOW_HEADERS = True
-# CORS_ORIGIN_ALLOW_ALL = True
+ACCESS_CONTROL_ALLOW_HEADERS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 CORS_ALLOWED_ORIGINS = [
@@ -176,28 +176,28 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
 ]
 
-# CORS_ALLOW_METHODS = (
-#     'DELETE',
-#     'GET',
-#     'OPTIONS',
-#     'PATCH',
-#     'POST',
-#     'PUT',
-# )
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
 
-# CSRF_TRUSTED_ORIGINS = (
-#     'http://localhost:3000',
+CSRF_TRUSTED_ORIGINS = (
+    'http://localhost:3000',
 
-# )
+)
 
-# CORS_ALLOW_HEADERS = (
-#     'accept',
-#     'accept-encoding',
-#     'authorization',
-#     'content-type',
-#     'dnt',
-#     'origin',
-#     'user-agent',
-#     'x-csrftoken',
-#     'x-requested-with',
-# )
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
