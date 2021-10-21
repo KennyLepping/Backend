682db20834a526793eeddf220070e9301173590c
Kenny Lepping <kennylepping@gmail.com>
2021-10-21 16:19:39 -0400

    Testing

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -26,6 +26,8 @@ DATABASES = {
 }
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'  # Original working S3 bucket: S3_BUCKET_NAME="zappa-a0rwj2q61"
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_ROOT = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # if you hosted your static files on a custom domain
 #AWS_S3_PUBLIC_URL_STATIC = "https://static.yourdomain.com/"
