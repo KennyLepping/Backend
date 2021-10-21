87974e3c5487e3b7b7ab61fbb3d8f574b0b9b6a4
Kenny Lepping <kennylepping@gmail.com>
2021-10-21 07:57:27 -0400

    Added DB

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -14,16 +14,16 @@ SECURE_SSL_REDIRECT = True # Try redirect if domain is added
 SECURE_HSTS_SECONDS = 31536000  # 1 year
 SECURE_HSTS_PRELOAD = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': getenv("DB_NAME"),
-        'USER': getenv("DB_USER"),
-        'PASSWORD': getenv("DB_PASSWORD"),
-        'HOST': getenv("DB_HOST"),
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': getenv("DB_NAME"),
+#         'USER': getenv("DB_USER"),
+#         'PASSWORD': getenv("DB_PASSWORD"),
+#         'HOST': getenv("DB_HOST"),
+#         'PORT': '5432',
+#     }
+# }
 
 # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'  # Original working S3 bucket: S3_BUCKET_NAME="zappa-a0rwj2q61"
 
