90892fb207d3397b2597d29d8325de76ad2d7a34
Kenny Lepping <kennylepping@gmail.com>
2021-10-21 07:58:13 -0400

    Added DB

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from os import getenv
 
+SECRET_KEY = getenv('SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
