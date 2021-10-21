69de4c1f1cdaa2bfd07e6898b1cee1c4b280e030
Kenny Lepping <kennylepping@gmail.com>
2021-10-21 08:38:08 -0400

    Adding the rest of the file

--- config/settings/base.py
+++ config/settings/base.py
@@ -161,6 +161,10 @@ AWS_S3_CUSTOM_DOMAIN = 'd28gmzqzeakcc8.cloudfront.net'
 # Use Cloudfront eventually, some other CDN, etc.
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+CORS_ALLOW_CREDENTIALS = True
+ACCESS_CONTROL_ALLOW_HEADERS = True
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 CORS_ALLOWED_ORIGINS = [
     'https://www.kennylepping.com',
@@ -168,3 +172,29 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
 ]
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CSRF_TRUSTED_ORIGINS = (
+    'http://localhost:3000',
+
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
