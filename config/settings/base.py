c4de3b5accfcb8c19bf2c47f9eaa4be9244d77c7
Kenny Lepping <kennylepping@gmail.com>
2021-10-21 23:54:12 -0400

    Deleted unnecessary CORS permissions

--- config/settings/base.py
+++ config/settings/base.py
@@ -173,35 +173,4 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
 ]
 
-# CORS_ALLOW_METHODS = (
-#     'DELETE',
-#     'GET',
-#     'OPTIONS',
-#     'PATCH',
-#     'POST',
-#     'PUT',
-# )
-
-# CSRF_TRUSTED_ORIGINS = (
-#     'https://www.kennylepping.com',
-#     'http://localhost:3000'
-# )
-
-# CORS_ORIGIN_WHITELIST = (
-#     'https://www.kennylepping.com',
-#     'http://localhost:3000'
-# )
-
-# CORS_ALLOW_HEADERS = (
-#     'accept',
-#     'accept-encoding',
-#     'authorization',
-#     'content-type',
-#     'dnt',
-#     'origin',
-#     'user-agent',
-#     'x-csrftoken',
-#     'x-requested-with',
-# )
-
 # EMAIL_BACKEND = 'django_ses.SESBackend'
